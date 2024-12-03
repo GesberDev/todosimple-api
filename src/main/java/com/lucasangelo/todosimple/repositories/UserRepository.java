d749e534992740d6e2def47abd6ad1802ad46486
Victor <victorgfmesquita@gmail.com>
2024-12-03 16:43:27 -0300

    Create UserController and refactor User.java UserRepository and pom.xml

--- src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
+++ src/main/java/com/lucasangelo/todosimple/repositories/UserRepository.java
@@ -3,6 +3,7 @@ package com.lucasangelo.todosimple.repositories;
 import com.lucasangelo.todosimple.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
